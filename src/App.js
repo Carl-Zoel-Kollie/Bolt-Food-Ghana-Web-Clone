154906e5b1283ab765579315f55af6bd1a5e5fd2
Zoel <carl@gmail.com>
2022-10-10 14:51:38 +0000

    Boltfirstcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import './style.css';
+import data from "./TemplateData.json";
+import { Link } from "react-router-dom";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="mycontainer">
+        <h1><span style={{color:"rgb(34 197 94)"}}>Bọlt</span> <span style={{color:"#111"}}>Food</span></h1>
+        <br/>
+        <h2>Delivery in <span style={{color:"rgb(34 197 94)"}}>Accra</span></h2>
+        <div className="searchInput_Container">
+          <input id="searchInput" type="text" placeholder="Restaurants or Cuisine" onChange={(event) => {
+            setSearchTerm(event.target.value);
+          }} />
+        </div>
+        <div className="template_Container">
+          {
+            data 
+              .filter((val) => {
+                if(searchTerm == ""){
+                  return val;
+                }else if(val.title.toLowerCase().includes(searchTerm.toLowerCase())){
+                  return val;
+                }
+              })
+              .map((val) => {
+                return(
+                  <div className="template" key={val.id}>
+                      <img src={val.image} alt="" />
+                      <h3>{val.title}</h3>
+                      <p className="price">GH₵{val.price}</p>
+                     <img src="star.png" style={{ width: '20px', height:"20px" ,marginTop:"-190px",marginLeft : 170, padding: 20 }}/>
+                     <h4>4.3</h4>
+                     <br></br>
+                     <Link to="/pay"><button>Order</button></Link>
+                  </div> 
+                )
+              })
+          }
+        </div>
+        
+      </div>
+      <div className="myfooter">
+          <h1>Order Now!</h1>
+        </div>
+    </>
+  )
 }
 
 export default App;
---
+++ src/PaystackIntegration.js
@@ -0,0 +1,99 @@
+import {useState} from 'react';
+import PaystackPop from "@paystack/inline-js";
+
+import "./Pay.css";
+
+const Pay=()=>{
+  
+    // const[phonenumber,setNumber]=useState("");
+    // const[location,setLocation]=useState("");
+    // const[amount,setAmount]=useState("");
+    
+    const[email,setEmail]=useState("");
+    const[amount,setAmount]=useState("");
+    const[firstName,setfirstName]=useState("");
+    const[lastName,setlastName ]=useState("");
+
+const payWithPaystack=(e)=>{
+  e.preventDefault();
+  const paystack= new PaystackPop();
+  paystack.newTransaction({
+    key:"pk_test_9868f2ae9b5f8b46cd63d97ec66dbc9623d5d707",
+    amount:amount * 100 ,
+    email,
+    firstName,
+    lastName,
+   
+    onSuccess(transaction){
+      let message=`Bolt Food is on the way. Reference ${transaction.reference}`
+      alert (message)
+      setEmail("")
+      setAmount("")
+      
+      
+    },
+    onCancel(){
+      alert("Order cancelled")
+    }
+
+  })
+
+}
+    return(
+        <div className="makepayment">
+            <h1><span style={{color:"rgb(34 197 94)"}}>Bọlt</span> <span style={{color:"#111"}}>Food</span></h1>
+          <section className="sectionthree">
+            hi</section>
+          <div className="mypayform">
+           
+             <form id="paymentForm" >
+ 
+  <div >
+  <input type="text" 
+    className="thisinput"
+    value={firstName}
+    placeholder="Enter your location(Bolt your deliver your food to this location)"
+    onChange={(e)=>setfirstName(e.target.value)}
+    id="first-name" />
+  </div>
+  <div >
+    
+    <input type="text"
+      placeholder="Enter your phone number(A bolt courier will call this number when they arrive with your food)"
+    className="thisinput" 
+    value={lastName}
+    onChange={(e)=>setlastName(e.target.value)}
+    id="last-name" />
+  </div>
+  <div>
+    
+    <input type="email" 
+    className="thisinput"
+    placeholder="Email(Optional means of being contacted)"
+    value={email}
+    onChange={(e)=>setEmail(e.target.value)}
+    id="email-address" required />
+  </div>
+  <div>
+    
+    <input type="tel"
+    className="thisinput"
+    placeholder="Enter amount you're being charged"
+    value={amount}
+    onChange={(e)=>setAmount(e.target.value)}
+     id="amount" required />
+  </div>
+  <div>
+    <button  type="submit" onClick={payWithPaystack}> Pay </button>
+  </div>
+</form>
+        </div>
+        <div className="myfooter">
+          <h1>Order Now!</h1>
+        </div>
+        </div>
+        
+
+    )
+}
+export default Pay;
